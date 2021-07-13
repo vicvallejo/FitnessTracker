32b4322fe73c5427f70a7e6ac6629d4edbe0cbcb
victor <victor.vallejo@tec.mx>
2021-07-13 13:12:10 -0500

    mongodb heroku deploy

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 require("./routes/api")(app);
